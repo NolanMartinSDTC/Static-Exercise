33e37a92cfceb0a6f5edc93ddd8ccc47d362aee2
Nolan Martin <nmartinsdtc@gmail.com>
2023-03-22 08:02:21 -0600

    added code for temp converter class with 2 mehtods

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("To convert from Fahrenheit to Celsius, enter 1\nTo convert from Celsius to Fahrenheit, enter 2\nOption 1, or option 2: ");
+            var userInput = int.Parse(Console.ReadLine());
+
+            while (userInput !=1 && userInput != 2)
+            {
+                Console.WriteLine("Please enter either 1 or 2: ");
+                userInput = int.Parse(Console.ReadLine());
+            }
+
+
+            if (userInput == 1)
+            {
+                Console.WriteLine("What temperature would you like to convert from Fahrenheit to Celsius? ");
+                var userTemp = Convert.ToDouble(Console.ReadLine());
+
+                double convTemp = TempConverter.FahrenheitToCelsius(userTemp);
+                Console.WriteLine($"{userTemp} degrees Fahrenheit is approximately {convTemp} degrees Celsius.");
+            }
+
+            else
+            {
+                Console.WriteLine("What temperature would you like to convert from Celsius to Fahrenheit? ");
+                var userTemp = Convert.ToDouble(Console.ReadLine());
+
+                double convTemp = TempConverter.CelsiusToFahrenheit(userTemp);
+                Console.WriteLine($"{userTemp} degrees Celsius is approximately {convTemp} degrees Fahrenheit.");
+
+            }
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace StaticExercise
+{
+	public static class TempConverter
+	{
+		
+		static TempConverter()
+		{
+		}
+
+		public static double FahrenheitToCelsius(double degInput)
+		{
+			Console.WriteLine($"Input temp: {degInput}");
+			double degOutput = ((degInput - 32) * (5d / 9d));
+			Console.WriteLine($"Output temp: {degOutput}");
+			return degOutput;
+		}
+
+		public static double CelsiusToFahrenheit(double degInput)
+		{
+			double degOutput = ((degInput * (9d / 5d)) + 32);
+			return degOutput;
+		}
+	}
+}
+
